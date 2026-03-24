bfd9d59766be1aada76da01d5019f7c2fd2389cc
nazariidatsenko25-png <nazarii.datsenko.25@kse.org.ua>
2026-03-24 18:31:19 +0200

    DeliveryItem

---
+++ Week8/DeliveryItem.cs
@@ -0,0 +1,23 @@
+namespace Week8;
+
+public abstract class DeliveryItem
+{
+    public string TrackingNumber { get; protected set; }
+    public double Weight { get; protected set; }
+    
+    public DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateDeliveryCost();
+    
+    
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"номере завмовлення: {TrackingNumber}, вага {Weight}");
+    }
+    
+}
