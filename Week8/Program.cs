ae61d74fa1998c01e6be41e9c7e350fc549bd84e
nazariidatsenko25-png <nazarii.datsenko.25@kse.org.ua>
2026-03-24 19:00:15 +0200

    Program

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Week8;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Letter letter1 = new Letter("123fsdf", 123);
+        Letter letter2 = new Letter("jhsofdigsdfg", 9999999);
+        Letter letter3 = new Letter("tuda", 123123);
+        Parcel parcel1 = new Parcel("su'da", 8080808, "30x100x150");
+        Parcel parcel2 = new Parcel("kuda", 6666, "77x77x77");
+        
+        letter1.PrintInfo();
+        parcel1.PrintInfo();
+        
+        CargoContainer<DeliveryItem> MyCargo = new CargoContainer<DeliveryItem>();
+        
+        MyCargo.AddItem(letter1);
+        MyCargo.AddItem(letter2);
+        MyCargo.AddItem(letter3);
+        MyCargo.AddItem(parcel1);
+        MyCargo.AddItem(parcel2);
+
+        double total = MyCargo.GetTotalCost();
+        Console.WriteLine(total);
+
+
+
+
+
+    }
+}
